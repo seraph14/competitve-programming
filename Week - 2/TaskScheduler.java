f0bccb19e1059e62d631c54a82617776b77512d3
Surafel <se.surafel.kassahun@gmail.com>
2021-08-28 10:38:54 +0300

    Update TaskScheduler.java

--- Week - 2/TaskScheduler.java
+++ Week - 2/TaskScheduler.java
@@ -1,15 +1,21 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] map = new int[26];
+        int max = 0, maxChar = 0;
         for (char c : tasks) {
             map[c - 'A']++;
+            if (map[c - 'A'] > max) {
+                max = map[c - 'A'];
+                maxChar = c - 'A';
+            }
         }
         
-        Arrays.sort(map);
-        int max = map[25] - 1;
+        max--;
         int idle_slots = max * n;
-        for (int i = 24; i >= 0; i--) {
-            idle_slots -= Math.min(map[i], max);
+        for (int i = 0; i < map.length; i++) {
+            if (i != maxChar) {
+                idle_slots -= Math.min(map[i], max);    
+            }
         }
         
         return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
