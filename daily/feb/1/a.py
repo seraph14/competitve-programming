a7e0b1e7d1681141c129227771f5bc0ee54a5e6c
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-02-02 00:04:31 +0300

    Update a.py

--- daily/feb/1/a.py
+++ daily/feb/1/a.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         count = blocks[:k].count("B")
-		minimum = k - count
+	minimum = k - count
 
-		for i in range(k, len(blocks)):
-			if blocks[i - k] == "B":
-				count -= 1
-			count += blocks[i] == "B"
-			minimum = min(minimum, k - count)
-		return minimum
+	for i in range(k, len(blocks)):
+		if blocks[i - k] == "B":
+			count -= 1
+		count += blocks[i] == "B"
+		minimum = min(minimum, k - count)
+	return minimum
